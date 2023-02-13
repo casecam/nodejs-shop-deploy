a62920ca34fd528a0b42f9273fe07b440df27752
casecam <109696610+casecam@users.noreply.github.com>
2023-02-13 15:38:04 -0800

    error controller fix

--- mvc/controllers/error.js
+++ mvc/controllers/error.js
@@ -1,3 +1,5 @@
 exports.handle404 =  (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res
+      .status(404)
+      .render('404', { pageTitle: 'Page Not Found', path: '/404' });
 }
