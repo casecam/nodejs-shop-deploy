9bf8497b2b23635eadd9ce93bb14699cf92032c4
casecam <109696610+casecam@users.noreply.github.com>
2023-02-20 13:22:57 -0800

    cook is set with login

--- auth/controllers/auth.js
+++ auth/controllers/auth.js
@@ -2,11 +2,11 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/');
-  };
+  req.session.isLoggedIn = true;
+  res.redirect('/');
+};
--- auth/controllers/shop.js
+++ auth/controllers/shop.js
@@ -8,7 +8,8 @@ exports.getProducts = (req, res, next) => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => {
@@ -21,9 +22,10 @@ exports.getProduct = (req, res, next) => {
   Product.findById(prodId)
     .then(product => {
       res.render('shop/product-detail', {
-        product: product,
+        product,
         pageTitle: product.title,
-        path: '/products'
+        path: '/products',
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -35,7 +37,8 @@ exports.getIndex = (req, res, next) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => {
@@ -52,7 +55,8 @@ exports.getCart = (req, res, next) => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: products
+        products,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -112,7 +116,8 @@ exports.getOrders = (req, res, next) => {
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
-        orders: orders
+        orders,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
