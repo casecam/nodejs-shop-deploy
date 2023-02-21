d0e17cfefd7b45e1986b691192493299dcc898db
casecam <109696610+casecam@users.noreply.github.com>
2023-02-20 16:26:39 -0800

    cookies complete

--- auth/controllers/auth.js
+++ auth/controllers/auth.js
@@ -13,9 +13,12 @@ exports.postLogin = (req, res, next) => {
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      res.redirect('/');
+      req.session.save((err) => {
+        console.error(err);
+        res.redirect('/');
+      });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.error(err));
 };
 
 exports.postLogout = (req, res, next) => {
